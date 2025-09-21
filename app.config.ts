087f365f88ad915b855cae44a4fa05121aade499
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:29:23 +0300

    add app configuration with profile and social links

---
+++ app.config.ts
@@ -0,0 +1,16 @@
+export default defineAppConfig({
+  profile: {
+    name: "Habib Shaban",
+    job: "Software Engineer",
+    email: "contact@habibshaban.dev",
+    avatar: "/images/avatar.png",
+    location: "Helsinki, Finland",
+    phone: "+358 44 952 9398",
+    isAvailable: true,
+    picture: "/images/me.jpeg",
+  },
+  socials: {
+    github: "https://github.com/habibshaban",
+    linkedin: "https://linkedin.com/in/habib-shaban",
+  },
+});
