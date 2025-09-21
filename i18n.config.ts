69a58ae2b96341781010c813e58c5097b4e16e0d
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:29:58 +0300

    refactor i18n configuration to use centralized messages import

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,14 +1,9 @@
-import en from "./locales/en.json";
-import fi from "./locales/fi.json";
+import messages from "~/locales/messages";
 
 export default defineI18nConfig(() => {
   return {
-    strategy: "no_prefix",
     legacy: false,
     locale: "en",
-    messages: {
-      en,
-      fi,
-    },
+    messages,
   };
 });
