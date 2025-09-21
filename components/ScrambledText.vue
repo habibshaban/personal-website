bfb37aea9504dcd34028be72d807e006026e14da
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:32:11 +0300

    feat: add ScrambledText component for animated text display on hover

---
+++ components/ScrambledText.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+}>()
+
+const displayText = ref(props.label)
+const charset = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+function randomChars(length: number) {
+  return Array.from(
+    { length },
+    () => charset[Math.floor(Math.random() * charset.length)]
+  ).join('')
+}
+
+async function scramble(input: string) {
+  let prefix = ''
+  for (let index = 0; index < input.length; index++) {
+    await new Promise((resolve) => setTimeout(resolve, 20))
+    prefix += input.charAt(index)
+    displayText.value = prefix + randomChars(input.length - prefix.length)
+  }
+}
+
+const startScrambling = () => {
+  scramble(props.label)
+  setTimeout(() => props.label.length * 50)
+}
+</script>
+
+<template>
+  <span class="text-nowrap" @mouseover="startScrambling">
+    {{ displayText }}
+  </span>
+</template>
