f892cfbf16e9157204a752d923eeb0193e82c68e
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:40:18 +0300

    feat: add LanguageToggle component to the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col bg-brandBackground relative">
+    <LanguageToggle class="fixed right-4 z-50 top-4" />
     <Navbar class="fixed bottom-0 z-50 flex sm:bottom-auto sm:top-0"/>
     <main class="flex-1">
       <slot />
