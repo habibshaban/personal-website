b0ea310d59c46601d373028054acca5d302ef417
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:37:30 +0300

    feat: add GradientLine component for customizable gradient lines with height and spacing options

---
+++ components/ui/GradientLine.vue
@@ -0,0 +1,50 @@
+<template>
+  <div 
+    :class="[
+      'gradient-line w-full bg-gradient-to-r from-transparent via-[#E5E7FF] to-transparent',
+      heightClass,
+      spacingClass
+    ]"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  height?: 'thin' | 'thick' | 'custom'
+  spacing?: 'none' | 'small' | 'medium' | 'large'
+  customHeight?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  height: 'thin',
+  spacing: 'medium'
+})
+
+const heightClass = computed(() => {
+  if (props.height === 'custom' && props.customHeight) {
+    return `h-[${props.customHeight}]`
+  }
+  
+  switch (props.height) {
+    case 'thick':
+      return 'h-[2px]'
+    case 'thin':
+    default:
+      return 'h-[0.5px]'
+  }
+})
+
+const spacingClass = computed(() => {
+  switch (props.spacing) {
+    case 'none':
+      return ''
+    case 'small':
+      return 'my-4'
+    case 'large':
+      return 'my-12'
+    case 'medium':
+    default:
+      return 'my-8'
+  }
+})
+</script>
