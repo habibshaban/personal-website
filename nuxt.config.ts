abfbd88f0886383f9d4b86f1fa6bfcd599abc5e9
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:30:08 +0300

    enhance nuxt configuration with meeting link, improved i18n settings, and additional modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      devtoolsEnabled: "",
+      meetingLink: process.env.NUXT_PUBLIC_MEETING_LINK || "https://cal.com/habibshaban",
     },
+  },
+
+  devServer: {
+    port: 3005,
   },
 
   modules: [
@@ -11,20 +15,80 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
     "@nuxt/image",
     "@nuxt/icon",
+    "@pinia/nuxt",
     "pinia-plugin-persistedstate/nuxt",
+    "shadcn-nuxt",
   ],
-  css: ["@/assets/style/app.scss", "~/assets/style/tailwind.css"],
+
+  imports: {
+    presets: [
+      {
+        from: "vue-sonner",
+        imports: ["toast"],
+      },
+    ],
+  },
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Geist:wght@100;200;300;400;500;600;700;800;900&family=Geist+Mono:wght@100;200;300;400;500;600;700;800;900&display=swap",
+        },
+      ],
+    },
+  },
+
+  piniaPluginPersistedstate: {
+    cookieOptions: {
+      maxAge: 3600 * 24 * 8,
+    },
+  },
+  css: ["@/assets/style/app.scss", "~/assets/style/tailwind.css", "vue-sonner/style.css"],
+
+  shadcn: {
+    prefix: "S",
+    componentDir: "./components/ui",
+  },
 
   components: [{ path: "./components", pathPrefix: false }],
-
   content: {
     build: {
       markdown: {
         highlight: {
           theme: "github-dark",
+          langs: [
+            "js",
+            "ts",
+            "vue",
+            "json",
+            "yaml",
+            "go",
+            "bash",
+            "css",
+            "scss",
+            "html",
+            "graphql",
+            "dockerfile",
+            "sql",
+            "powershell",
+            "python",
+          ],
         },
       },
     },
+    locales: ["en", "fi"],
+    defaultLocale: "en",
   },
 
   postcss: {
@@ -37,32 +101,40 @@ export default defineNuxtConfig({
 
   i18n: {
     locales: [
-      { code: "en", name: "English" },
-      { code: "fi", name: "Suomi" },
+      { code: "en", name: "English", language: "en-US" },
+      { code: "fi", name: "Suomi", language: "fi-FI" },
     ],
+    strategy: "prefix",
+    detectBrowserLanguage: {
+      useCookie: true,
+      alwaysRedirect: true,
+      cookieKey: "i18n_redirected",
+      redirectOn: "all",
+    },
     defaultLocale: "en",
-    vueI18n: "./i18n.config.ts",
+    vueI18n: "~/i18n.config.ts",
   },
 
   icon: {
     customCollections: [
       {
         prefix: "custom",
-        dir: "./app/assets/icons",
+        dir: "./assets/icons",
       },
     ],
     clientBundle: {
       scan: true,
       includeCustomCollections: true,
     },
-    provider: "iconify",
+    // provider: "iconify",
+    collections: ["solar", "tabler", "mdi"],
   },
 
-  devtools: { enabled: process.env.NUXT_PUBLIC_DEVTOOLS_ENABLED },
+  devtools: { enabled: process.env.NUXT_PUBLIC_DEVTOOLS_ENABLED === "true" },
 
   compatibilityDate: "2025-01-22",
 
-  image: {
-    dir: "assets/",
-  },
+  // image: {
+  //   provider: "ipx",
+  // },
 });
