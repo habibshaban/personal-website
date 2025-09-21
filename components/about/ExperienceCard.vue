1272539d0682722a936329d55c0277a96c927479
Habib Shaban <h.shaban234@yahoo.com>
2025-09-21 17:32:27 +0300

    feat: add ExperienceCard component for displaying user experiences

---
+++ components/about/ExperienceCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { ExperienceItem } from '~/lib/interfaces/profile-interfaces';
+
+
+interface Props {
+  items: ExperienceItem[]
+}
+withDefaults(defineProps<Props>(), {
+})
+</script>
+
+<template>
+  <section class="rounded-3xl h-full bg-[#121212]/80 p-6 ring-1 ring-white/10 md:p-8">
+
+      <h3 class="text-xl font-semibold tracking-tight text-white mb-5">{{ $t('about.experiences') }}</h3>
+
+      <ul class="space-y-4">
+        <li v-for="(it, i) in items" :key="i" class="grid grid-cols-[1fr,auto] gap-x-4">
+          <div>
+            <p class="font-medium text-white/90">{{ it.role }}</p>
+            <p class="text-white/60 text-sm">{{ it.company }}</p>
+          </div>
+          <span class="text-white/50 text-sm whitespace-nowrap self-start">{{ it.dates }}</span>
+        </li>
+      </ul>
+
+  </section>
+</template>
