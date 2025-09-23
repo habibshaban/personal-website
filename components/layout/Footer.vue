210278a23edf652e7c6b2015e2ef2ebd05584dc7
Habib Shaban <h.shaban234@yahoo.com>
2025-09-23 08:28:31 +0300

    fix: fixing footer visibility in mobile

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="w-full p-4 bg-transparent text-[#AAAAAA] text-center text-sm/6">
+  <footer class="w-full p-4 pb-16 sm:pb-4 bg-transparent text-[#AAAAAA] text-center text-sm/6">
     <p>© 2025 My Website. All Rights Reserved.</p>
   </footer>
 </template>
